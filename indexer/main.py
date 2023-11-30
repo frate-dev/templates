a0383ee60a8fcf8203a2b96258d6b9fcc5ca1943
lucas sprouole <lucas.sproule.42@gmail.com>
2023-11-29 21:08:43 -0500

    github actions

--- indexer/main.py
+++ indexer/main.py
@@ -1,10 +1,14 @@
 import pathlib
-from json import dumps
+from json import dumps, load
 
 sources = []
 for dir in pathlib.Path('../srcs').iterdir():
     if dir.is_dir():
-        sources.append({"name": dir.name})
+        file = pathlib.Path(dir)/'index.json'
+        if not (file.exists()):
+            print(f"Warning: {file} does not exist")
+        with open(file, 'r') as f:
+            sources.append(load(f))
 
 with open('../index/index.json', 'w') as f:
     f.write(dumps(sources, indent=2))
